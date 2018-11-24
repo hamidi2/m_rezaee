f632a6784717d5762148a510b5668f06a209c838
hamidi <hamidi@gmail.com>
2018-11-24 10:46:05 +0330

    970903_1046

--- WindowsFormsApplication1/WindowsFormsApplication1/List.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/List.cs
@@ -34,7 +34,7 @@ namespace WindowsFormsApplication1
 			var builder = new SQLiteCommandBuilder(_ad);
 			_ad.DeleteCommand = builder.GetDeleteCommand();
 			_ad.InsertCommand = builder.GetInsertCommand();
-			_ad.SelectCommand = builder.GetUpdateCommand();
+			_ad.UpdateCommand = builder.GetUpdateCommand();
 			_ad.Fill(_dt);
 	        btnEdit.Enabled = btnRemove.Enabled = _dt.Rows.Count != 0;
 			listBox1.DataSource = _dt;
@@ -50,24 +50,24 @@ namespace WindowsFormsApplication1
 		private void btnAdd_Click(object sender, EventArgs e)
 		{
 			_dt.Rows.Add();
-			Update();
+			Debug.Assert(listBox1.SelectedItem is DataRowView);
+			if (new StudentsForm2(listBox1.SelectedItem as DataRowView).ShowDialog(this) == DialogResult.OK)
+				Update();
+			else
+				_dt.Rows.RemoveAt(listBox1.SelectedIndex);
+		}
+
+		private void btnEdit_Click(object sender, EventArgs e)
+		{
+			Debug.Assert(listBox1.SelectedItem is DataRowView);
+			if (new StudentsForm2(listBox1.SelectedItem as DataRowView).ShowDialog(this) == DialogResult.OK)
+				Update();
 		}
 
 		private void btnRemove_Click(object sender, EventArgs e)
 		{
 			_dt.Rows.RemoveAt(listBox1.SelectedIndex);
 			Update();
-		}
-
-		private void btnEdit_Click(object sender, EventArgs e)
-		{
-			Debug.Assert(listBox1.SelectedItem is DataRowView);
-			var f = new StudentsForm2(listBox1.SelectedItem as DataRowView);
-			if (f.ShowDialog(this) == DialogResult.OK)
-			{
-				_ad.Update(_dt);
-				Update();
-			}
 		}
 
 		private void List_KeyDown(object sender, KeyEventArgs e)
